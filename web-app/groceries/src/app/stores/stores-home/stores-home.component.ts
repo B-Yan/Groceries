7f6bdc0f3114fe2030af1b004e7c33b24623c32e
Yannick Bellerose <yan-bel@hotmail.com>
2021-06-09 14:14:34 -0400

    a working call to the backend

--- web-app/groceries/src/app/stores/stores-home/stores-home.component.ts
+++ web-app/groceries/src/app/stores/stores-home/stores-home.component.ts
@@ -14,44 +14,16 @@ export class StoresHomeComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    //this.search().subscribe((response: any) => {
-      //this.stores = response.query.search;
-    //});
+  ngOnInit(): void {  
+    this.search().subscribe((response: any) => {this.stores = response;});
+  }
 
-    $.support.cors = true;
-    $.ajax({
-      type: 'GET',
-      url: 'http://localhost:8080/api/stores',
-      success: function(storeArray) {
-        console.log(storeArray);
-        this.stores = new Array(storeArray.length);
-        for (let i = 0; i<storeArray.length; i++){
-          var stor = {name: storeArray[i].name, address: storeArray[i].address, phone: storeArray[i].phoneNumber};
-          this.stores[i] = stor;
-        }
-      },
-      error: function() {
-          alert("CAN'T CONNECT TO DATABSE!");
-      }
-  })
-
-    //let store1 = {name: 'Test1', openAt: 11, closeAt: 20, address: '1111 Test Street', phone: '123 113-412'};
-    //let store2 = {name: 'Test2', openAt: 15, closeAt: 22, address: '2222 Test Drive', phone: '222 123-432'};
-    //this.stores = [store1, store2];
+  public search() {
+    return this.http.get('http://localhost:8080/api/stores');
   }
 
   storeIsOpen(openAt: number, closeAt:number): boolean{
     var date = new Date();
     return openAt <= date.getHours() && date.getHours() < closeAt;
   }
-
-  //public search() {
-    //return this.http.get('localhost:8080/api/stores', {
-      //params: {
-        //action: 'GET',
-        //format: 'json'
-      //}
-    //});
-  //}
 }
