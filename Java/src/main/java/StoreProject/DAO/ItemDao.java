7e57f52e08ab96c9c98df807a9de2a0483151e01
Chloe Sofia Encenzo <encenzochloe@gmail.com>
2021-06-09 09:48:30 -0700

    Added GET request for all items

--- Java/src/main/java/StoreProject/DAO/ItemDao.java
+++ Java/src/main/java/StoreProject/DAO/ItemDao.java
@@ -7,6 +7,7 @@ import StoreProject.DTO.ItemFormatted;
 
 public interface ItemDao 
 {
+	List<ItemFormatted> getItems();
 	List<ItemFormatted> getItemsByCategory(Integer categoryID); //in controller categoryid will be passed
 	List<ItemFormatted> getItemsOnSale();
 }
