9626e1b3b0abe551a08e2e1464e4c44c4c8b334e
joaoryff <joaoryff@gmail.com>
2021-06-09 21:15:46 -0400

    Update item component
    Shows the old price striked and the new price when is sales.
    Show a tag with the discont.

--- web-app/groceries/src/app/item/item-entity/item-entity.component.ts
+++ web-app/groceries/src/app/item/item-entity/item-entity.component.ts
@@ -11,18 +11,32 @@ export class ItemEntityComponent implements OnInit {
 
   items: any;
   isSale: boolean = true;
+  isVisible = 'none';
 
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
     this.search().subscribe((response: any) => {this.items = response;});
   }
   public search() {
-    return this.http.get('http://localhost:8080/api/items');
+    return this.http.get('http://localhost:8090/api/items');
   }
 
   hasDiscount(item: any) {
+    
     return  item > 0 ? this.isSale = true : this.isSale = false
   }
+  newPrice(item:any){
+    return item.price*(1-item.percentageOFF)
+  }
+
+  itVisible(item: any) {
+    console.log(this.isVisible, item.percentageOFF)
+
+    return  item > 0 ? this.isVisible = 'block' : this.isVisible = 'none'
+   
+  }
+
+
 
 
 }
